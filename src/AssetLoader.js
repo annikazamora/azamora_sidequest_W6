00bb99dcf234d3715283cb78f7d3d986cbbc7527
annikazamora <azamora@uwaterloo.ca>
2026-03-08 14:07:03 -0400

    added bell file

--- src/AssetLoader.js
+++ src/AssetLoader.js
@@ -211,6 +211,7 @@ async function loadBackgrounds(levelPkg) {
     bgFar: await loadImageAsync("assets/background_layer_1.png"),
     bgMid: await loadImageAsync("assets/background_layer_2.png"),
     bgFore: await loadImageAsync("assets/background_layer_3.png"),
+    bgExtra: await loadImageAsync("assets/background_layer_4.png"), // Added fourth layer
   };
 }
 
